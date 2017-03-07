1875101f2a048f881aa14971fc1d8b457d1c427c
Matthias Freund <matthias_freund@outlook.com>
2017-03-07 11:57:58 +0100

    Bewegung

--- domination.pl
+++ domination.pl
@@ -4,4 +4,13 @@ use robot;
 
 my$willi=robot->new("Willi");
 $willi->links_hoch;
-my$pos=$willi->links_pos;
+
+my$pos=$willi->{pos};
+print "$willi->{pos}[0] \n";
+print "$willi->{pos}[1] \n";
+$willi->schritt_rechts();
+
+$pos=$willi->{pos};
+
+print "$willi->{pos}[0] \n";
+print "$willi->{pos}[1] \n";
--- robot.pm
+++ robot.pm
@@ -10,6 +10,8 @@ sub new
     my$robot={rechts_arm=>'unten',
 	      links_arm=>'unten',
 	      name=>$name,
+	      pos=>[0,0]
+
 	      };
     bless($robot, 'robot');
     return $robot;
@@ -26,5 +28,30 @@ sub links_pos
     my$robot=$_[0];
     return $robot->{links_arm};
 }
+
+sub links_runter
+{
+    my$robot=$_[0];
+    $robot->{links_arm}='unten';
+}
+sub rechts_hoch
+{
+    my$robot=$_[0];
+    $robot->{rechts_arm}='hoch';
+}
+sub rechts_runter
+{
+    my$robot=$_[0];
+    $robot->{links_arm}='unten';
+}
+
+sub schritt_rechts
+{
+ my$robot=$_[0];
+ $robot->{pos}[0]=$robot->{pos}[0]+1;
+}
+
+
+
 
 1;
