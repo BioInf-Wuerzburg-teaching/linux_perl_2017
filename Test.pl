3213efebe746cf65118fc8b9bf8b3fa77e9a7fab
Jan Freudenthal <jan.freudenthal@uni-wuerzburg.de>
2017-03-08 10:51:34 +0100

    Neue Test Sex Test eingefügt

--- Test.pl
+++ Test.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests=>8;
+use Test::More tests=>9;
 
 require_ok('robot');
 
@@ -8,8 +8,13 @@ my$bob  = robot -> new('bob');
 
 my$name = $bob->name();
 
-is($name,'klaus','nametest');
+
+is($name,'bob','nametest');
 is($name,'bob','nametest_2');
+
+my$sex = $bob->sex();
+is($sex,'Apache','Gendertest');
+
 
 can_ok('robot','move_rechts');
 can_ok('robot','move_links');
@@ -17,3 +22,4 @@ can_ok('robot','move_runter');
 can_ok('robot','hochlaufen');
 can_ok('robot','runterlaufen');
 can_ok('robot','Welt_Zerstoeren');
+can_ok('robot','Bier_trinken');
