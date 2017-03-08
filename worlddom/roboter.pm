1568d9ed11f9c035e533f824dda807bb20bc26d3
Matthias Freund <matthias_freund@outlook.com>
2017-03-08 16:07:01 +0100

    redundanz entfernt

--- worlddom/roboter.pm
+++ worlddom/roboter.pm
@@ -11,11 +11,120 @@ sub new
 		hand_re => 'offen',
 		name    => $name,
 		pos     => [0,0],
-		Geschlecht => ''};
+		Geschlecht => 'XX'};
     bless $roboter,'roboter';
     return $roboter;
  
 }
+
+
+#---------------------------------------------------------------------------------------
+
+sub arm
+{
+    my $roboter,$arm, $richtung = @_;
+
+    if ($arm eq 'links')
+    {
+    
+	if ($richtung eq 'hoch')
+	{ 
+	    $roboter -> {arm_li} = 'oben';
+	}
+    
+	elsif ($richtung eq 'runter')
+	{
+	    $roboter -> {arm_li} = 'unten';
+	}
+
+	else ($richtung ne 'hoch'| $richtung ne 'runter') {print "angegebener Parameter für Richtung ist weder 'hoch' noch 'runter'";
+	}
+    }
+
+ elsif ($arm eq 'rechts')
+    {
+    
+	if ($richtung eq 'hoch')
+	{ 
+	    $roboter -> {arm_re} = 'oben';
+	}
+    
+	elsif ($richtung eq 'runter')
+	{
+	    $roboter -> {arm_re} = 'unten';
+	}
+
+	else ($richtung ne 'hoch'| $richtung ne 'runter')
+	
+	{
+	    print "angegebener Parameter für Richtung ist weder 'hoch' noch 'runter'";
+	}
+    }
+    else ($arm ne 'links'| $arm ne 'rechts')
+    {
+	print "angegebener Parameter für Arm ist weder 'links' noch 'rechts' ";	
+    }
+}
+
+#------------------------------------------------------------------------------------------------------------
+  
+sub hand
+{
+    my $roboter,$hand, $zustand = @_;
+
+    if ($hand eq 'links')
+    {
+    
+	if ($zustand eq 'auf')
+	{ 
+	    $roboter -> {hand_li} = 'offen';
+	}
+    
+	elsif ($zustand eq 'zu')
+	{
+	    $roboter -> {hand_li} = 'geschlossen';
+	}
+
+	else ($zustand ne 'auf'| $zustand ne 'zu') {print "angegebener Parameter für Richtung ist weder 'auf' noch 'zu'";
+	}
+    }
+
+ elsif ($hand eq 'rechts')
+    {
+    
+	if ($zustand eq 'auf')
+	{ 
+	    $roboter -> {hand_re} = 'offen';
+	}
+    
+	elsif ($zustand eq 'zu')
+	{
+	    $roboter -> {hand_re} = 'geschlossen';
+	}
+
+	else ($zustand ne 'auf'| $zustand ne 'zu')
+	
+	{
+	    print "angegebener Parameter für Richtung ist weder 'auf' noch 'zu'";
+	}
+    }
+    else ($hand ne 'links'| $hand ne 'rechts')
+    {
+	print "angegebener Parameter für Hand ist weder 'links' noch 'rechts' ";	
+    }
+}
+
+
+
+1;
+    
+    
+
+
+
+
+
+=pod
 
 sub arm_li_hoch
 {
@@ -98,5 +207,6 @@ sub pos_hand_li
     my $roboter = $_[0];
     return $roboter -> {hand_li};
 }
+=cut
 
 1;
