9313fbfe3e7d9c0064c3275d6bde37e1c10268ec
biofranzi <franziska.saul@stud-mail.uni-wuerzburg.de>
2017-03-09 16:50:42 +0100

    Test für att_change geschrieben (pos ausgelassen)

--- Testing.pl
+++ Testing.pl
@@ -8,6 +8,13 @@ use_ok('roboter');
 can_ok('roboter','new');
 my$bob = new_ok('roboter'=>['roboter']);
 
+can_ok('roboter','att_change');
+is($bob->att_change("arm_li","oben"),"oben","arm_links_oben");
+is($bob->att_change("arm_re","oben"),"oben","arm_rechts_oben");
+is($bob->att_change("hand_li","auf"),"auf","hand_links_auf");
+is($bob->att_change("hand_re","auf"),"auf","hand_rechts_auf");
+is($bob->att_change("Geschlecht","XY"),"XY","Gechlecht_geändert");
+
 can_ok('roboter','bewegen_l');
 
 is($bob->bewegen_l(),"-1","links_Test");
@@ -23,11 +30,7 @@ is($bob->bewegen_zur(),"0","links_Test");
 
 can_ok('roboter','zeig_dich');
 
- 
-
 can_ok('roboter','Geschlechtsumwandlung');  
 can_ok('roboter','Referendum');
-#is('  
-
 
 done_testing()
