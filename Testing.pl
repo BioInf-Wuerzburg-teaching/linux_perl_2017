3a5ae6060989a5e9fe6719ab1d303172adf68330
Matthias Freund <matthias_freund@outlook.com>
2017-03-09 16:45:07 +0100

    tests fuer hand() und arm() hinzugefuegt

--- Testing.pl
+++ Testing.pl
@@ -8,6 +8,60 @@ use_ok('roboter');
 can_ok('roboter','new');
 my$bob = new_ok('roboter'=>['roboter']);
 
+my @methodlist = ('arm', 'hand', 'att_change');
+can_ok('roboter', @methodlist);
+
+is($bob->{arm_li},'unten', 'yay'); 
+
+$bob->arm('links', 'hoch');
+
+is($bob->{arm_li},'oben', 'yay');
+
+$bob->arm('links', 'runter');
+
+is($bob->{arm_li},'unten', 'yay'); 
+
+
+
+
+is($bob->{hand_li},'offen', 'yay'); 
+
+$bob->hand('links', 'zu');
+
+is($bob->{hand_li},'geschlossen', 'yay');
+
+$bob->hand('links', 'auf');
+
+is($bob->{hand_li},'offen', 'yay'); 
+
+
+
+is($bob->{arm_re},'unten', 'yay'); 
+
+$bob->arm('rechts', 'hoch');
+
+is($bob->{arm_re},'oben', 'yay');
+
+$bob->arm('rechts', 'runter');
+
+is($bob->{arm_re},'unten', 'yay'); 
+
+
+
+
+is($bob->{hand_re},'offen', 'yay'); 
+
+$bob->hand('rechts', 'zu');
+
+is($bob->{hand_re},'geschlossen', 'yay');
+
+$bob->hand('rechts', 'auf');
+
+is($bob->{hand_re},'offen', 'yay'); 
+
+
+
+
 can_ok('roboter','bewegen_l');
 
 is($bob->bewegen_l(),"-1","links_Test");
