b57a03295602ec87bd65c07b1742358a884a3cf7
biofranzi <franziska.saul@stud-mail.uni-wuerzburg.de>
2017-03-10 14:22:11 +0100

    Test arm_rechts_oben angelegt

--- Testing.pl
+++ Testing.pl
@@ -11,4 +11,5 @@ my $bob = new_ok('roboter'=>['bob']);
 can_ok('roboter','arm');
 is($bob->arm("links","oben"),"oben","arm_links_oben");
 is($bob->arm("links","unten"),"unten","arm_links_unten");
+is($bob->arm("rechts","oben"),"oben","arm_rechts_oben");
 done_testing()
