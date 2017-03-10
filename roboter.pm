3602ab4b55cac2c91f1ba92c76730f3eef31b6da
biofranzi <franziska.saul@stud-mail.uni-wuerzburg.de>
2017-03-10 13:58:28 +0100

    sub arm() hinzugefügt

--- roboter.pm
+++ roboter.pm
@@ -2,34 +2,78 @@ package roboter;
 use strict;
 use warnings;
 
+=head1 Cooler Roboter
+
 =pod
 
 Dies ist ein Roboter, der folgende Attribute hat:
-- einen linken Arm (arm_li), der oben oder unten sein kann
-- einen rechten Arm (arm_re), der oben oder unten sein kann
-- eine linke Hand (hand_li), die offen oder zu sein kann
-- eine rechte Hand (hand_re), die offfen oder zu sein kann
-- einen Namen
-- eine Position (pos), die in der Form [X,Y] angegeben wird
-- ein Geschlecht, dass entweder weiblich (XX) oder männlich (XY)
 
 =cut
 
+=over
+   
+=item
+
+einen linken Arm (arm_li), der oben oder unten sein kann
+
+=cut
+
+=item
+    
+einen rechten Arm (arm_re), der oben oder unten sein kann
+
+=cut
+
+=item
+
+eine linke Hand (hand_li), die offen oder zu sein kann
+
+=cut
+
+=item
+
+eine rechte Hand (hand_re), die offfen oder zu sein kann
+
+=cut
+
+=item
+
+einen Namen
+
+=cut
+
+=item
+
+eine Position (pos), die in der Form [X,Y] angegeben wird
+
+=cut
+
+=item
+
+ein Geschlecht, dass entweder weiblich (XX) oder maennlich (XY)
+
+=cut
+
+=back
+
+=cut    
+
 sub new
 {
- my $name = $_[1];   
- my $roboter = {arm_li  => 'unten',
-	        arm_re  => 'unten',
-		hand_li => 'offen',
-		hand_re => 'offen',
-		name    => $name,
-		pos     => [0,0],
-		Geschlecht => 'XX'};
-
+    my $name = $_[1];   
+    my $roboter = {"arm_li"  => 'unten',
+		   "arm_re"  => 'unten',
+		   "hand_li" => 'offen',
+		   "hand_re" => 'offen',
+		   "name"    => $name,
+		   "pos"     => [0,0],
+		   "Geschlecht" => 'XX'};
     bless $roboter,'roboter';
-
     return $roboter;
- 
+}
+
+sub arm
+{
 }
 
 1;
