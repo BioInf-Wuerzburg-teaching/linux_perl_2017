5fbb28ae75f7865dc67ccc5f65d445fb4458e385
Jan Freudenthal <jan.freudenthal@uni-wuerzburg.de>
2017-03-10 17:15:43 +0100

    Test fuer move hizugefügt

--- robot/Testing_Schnell.pl
+++ robot/Testing_Schnell.pl
@@ -7,4 +7,8 @@ my $fast_bob=new_ok('robot::schnell'=>['petra']);
 
 can_ok('robot::schnell','move');
 
+my @output = $fast_bob->move();
+my @expected = (0,0);
+is_deeply(\@output, \@expected, 'MoveTest');
+
 done_testing();
