6e1c86109385b2124870acd50224824ee0850839
biofranzi <franziska.saul@stud-mail.uni-wuerzburg.de>
2017-03-10 13:57:39 +0100

    Test ob sub arm() exestiert angelegt

--- Testing.pl
+++ Testing.pl
@@ -1,10 +1,12 @@
 use strict;
 use warnings;
+use Data::Dumper;
 
 use Test::More; 
 
 use_ok('roboter');
 can_ok('roboter','new');
-my$bob = new_ok('roboter'=>['roboter']);
+my $bob = new_ok('roboter'=>['bob']);
 
+can_ok('roboter','arm');
 done_testing()
